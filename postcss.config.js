aae99e5f202b3e225c0744ed283a611e80a1f874
VitFL <vitfl@protonmail.com>
2018-12-18 17:26:33 +0200

    :wrench: configuration file for PostCSS

---
+++ postcss.config.js
@@ -0,0 +1,5 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable global-require */
+module.exports = {
+  plugins: [require('autoprefixer')], // PostCSS plugin to parse CSS and add vendor prefixes to CSS rules using values from https://caniuse.com
+};
