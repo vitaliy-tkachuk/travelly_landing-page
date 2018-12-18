bdcbb88733afd1cb4d9c8409926ecc0629fd4606
VitFL <vitfl@protonmail.com>
2018-12-18 17:30:32 +0200

    :sparkles: basic app structure: fonts,img, js,scss and index.html(empty for now)

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-alert */
+/* eslint-disable func-names */
+
+// your custom code goes below...
+let $ = document.getElementById.bind(document);
+
+$('doAlert').onclick = function() {
+  $('time').textContent = new Date();
+  alert('Alert !');
+};
